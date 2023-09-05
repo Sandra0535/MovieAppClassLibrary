f5e9169efba15282de150698c528092654eb624b
Aurionpro <Aurionpro@ACER1208>
2023-09-05 12:04:08 +0530

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,22 @@
+﻿namespace MovieAppClassLibrary
+{
+    public class Movie
+    {
+        private int _id;
+        private string _name;
+        private int _year;
+        private string _genre;
+        public int Id { get { return _id; } set { _id = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+        public int Year { get { return _year; } set { _year = value; } }
+        public string Genre { get { return _genre; } set { _genre = value; } }
+        public Movie() { }
+        public Movie(int id, string name, int year, string genre)
+        {
+            _id = id;
+            _name = name;
+            _year = year;
+            _genre = genre;
+        }
+    }
+}
---
+++ Service/MovieStoreFullException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieAppClassLibrary
+{
+    public class MovieStoreFullException:Exception
+    {
+        public MovieStoreFullException(string message) : base(message) { }
+    }
+}
